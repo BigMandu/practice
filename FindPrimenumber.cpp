d72bd929a3b8cd429daef24c73cd160525a2f396
BigMandu <2only1@naver.com>
2021-02-14 03:23:45 +0900

    Update FindPrimenumber.cpp

--- FindPrimenumber.cpp
+++ FindPrimenumber.cpp
@@ -10,13 +10,52 @@ n	result
 5	3
 
 */
+#include <iostream>
 
 #include <string>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
+
+    vector<bool>prime(n,true);
+
+    prime[0] = prime[1] = false;
+
+
+    
+    for(int i = 2; i<= int(sqrt(n)); i++)
+    {
+        if(prime[i])
+        {
+            for(int j = i*i; j<=n; j+=i)
+            {
+                prime[j] = false;
+            }
+        }
+    }
+
+    for(bool x : prime)
+    {
+        if(x)
+        {
+            answer++;
+        }
+    }   
+
+
     return answer;
+}
+
+int main()
+{
+    int n;
+    cin>> n;
+
+    n = solution(n);
+
+    cout << n;
 }
