d54afd82fd2d5f29e59b6eef5df7a39014872796
BigMandu <2only1@naver.com>
2021-02-23 13:59:54 +0900

    complete

--- matrixadd.cpp
+++ matrixadd.cpp
@@ -2,6 +2,7 @@
 https://programmers.co.kr/learn/courses/30/lessons/12950
 
 */
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -10,14 +11,16 @@ using namespace std;
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) 
 {
     vector<vector<int>> answer;
+
+    answer.resize(arr1.size());
     
     for(int i = 0; i< arr1.size(); i++)
     {
         for(int j = 0; j<arr1[0].size(); j++)
         {
-            vector<int> x;
-            x.push_back(arr1.at(i).at(j) + arr2.at(i).at(j));
-            answer.push_back(x);
+            int x;
+            x = arr1.at(i).at(j) + arr2.at(i).at(j);
+            answer[i].push_back(x);
         }
     }
 
@@ -29,16 +32,16 @@ int main()
     vector<vector<int>>vec1;
     vector<vector<int>>vec2;
 
-    vec1 = {{1,2,3}, {2,3,3}};
+    vec1 = {{1,2}, {2,3}};
     vec2 = {{3,4}, {5,6}};
 
-    vec1 = solution(vec1, vec2);
+    vec1 = solution(vec1, vec2); //4,6 7,9
     
     for(int i = 0; i< vec1.size(); i++)
     {
         for(int j = 0; j<vec1[0].size(); j++)
         {
-             printf("%d ", vec1.at(0).at(i));
+            cout << vec1[i][j];
         }
     }
 }
