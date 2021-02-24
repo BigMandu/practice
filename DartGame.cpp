9c8a3f086557accc06ad34f707c2a45cf53a4ff7
BigMandu <2only1@naver.com>
2021-02-24 20:27:53 +0900

    dartgame add

--- DartGame.cpp
+++ DartGame.cpp
@@ -139,3 +139,29 @@ int main()
     x = solution(str);
     cout << x;
 }
+
+/* 다른사람이 푼거 ㅇㅇ
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string dartResult) {
+    vector<int> v;
+    for(int i = 0, s = 0; i < dartResult.size(); ++i) {
+        if(dartResult[i] >= '0' && dartResult[i] <= '9')
+            s = s * 10 + dartResult[i] - '0';
+        else if(dartResult[i] == 'S') v.push_back(s), s = 0;
+        else if(dartResult[i] == 'D') v.push_back(s * s), s = 0;
+        else if(dartResult[i] == 'T') v.push_back(s * s * s), s = 0;
+        else if(dartResult[i] == '*') {
+            if(v.size() > 1) v.back() *= 2, v[v.size() - 2] *= 2;
+            else v.back() *= 2;
+        } else v.back() = -v.back();
+
+    }
+    int ans = 0;
+    for(int i = 0; i < v.size(); ++i) ans += v[i];
+    return ans;
+}
+*/
